067150d75dc451b808cacb0bf1ca57511eb4adbb
root <med.work96@hotmail.com>
2024-10-08 19:41:25 +0000

    Task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+/**
+ * Retireves a list of students
+ *
+ * @returns [{id: number, firstName: String, location: String}]
+ */
+
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
